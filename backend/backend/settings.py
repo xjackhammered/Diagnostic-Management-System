070c24ba3609a825d2fd6cbf5735575015234055
xjackhammered <hammerofjack7@gmail.com>
2026-04-28 17:39:51 +0600

    added the DB_password in settings.py in Databases

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'diagnostic_db'),
         'USER': os.getenv('DB_USER', 'diagnostic_user'),
-        'USER': os.getenv('DB_USER', 'diagnostic_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }

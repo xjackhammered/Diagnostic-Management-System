2a1c7d2973df66bdd795bd7582c813ab837ae672
xjackhammered <hammerofjack7@gmail.com>
2026-04-29 16:59:48 +0600

    Added models to admin panel to see how they work

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . models import Patient, Doctor, Collaborator, CollaboratorTest, Booking, BookingItem, Diagnostic
 # Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Collaborator)
+admin.site.register(CollaboratorTest)
+admin.site.register(Booking)
+admin.site.register(BookingItem)
+admin.site.register(Diagnostic)
